6b00ccae2e0d53541d1b68f8e4b56db076298417
Collin Bachman <collin@bachman.io>
2022-09-15 16:04:39 -0400

    Improve Message Delete Logic (#16)
    * Move typedefs and improve message deletion logic
    
    * Include Audit Log reason in message deletes
    
    Signed-off-by: Collin Bachman <collin@bachman.io>

--- src/functions/discord/deletePreviousMessages.ts
+++ src/functions/discord/deletePreviousMessages.ts
@@ -1,35 +1,50 @@
-import type {
-	RESTGetAPIChannelMessagesResult,
-	RESTPostAPIChannelMessagesBulkDeleteJSONBody,
-} from "discord-api-types/v10";
+import type { Env, RESTGetAPIChannelMessagesResult, RESTPostAPIChannelMessagesBulkDeleteJSONBody } from "../../types";
 import { APIVersion } from "../../const/discord/ourAPIVersion";
-import type { Env } from "../../types";
 
 export default async function deletePreviousMessages(
 	messages: RESTGetAPIChannelMessagesResult,
 	env: Env,
 ): Promise<void> {
 	const emptyResponse = 204;
-	const headers = new Headers({
-		Authorization: `Bot ${env.DISCORD_BOT_TOKEN}`,
-		"Content-Type": "application/json",
-	});
+	const auditLogReason = "Regular Channel Message Cleanup by Lamar (BOT)";
 
-	const messageIds: string[] = [];
-	messages.forEach((message) => {
-		messageIds.push(message.id);
-	});
-	const payload: RESTPostAPIChannelMessagesBulkDeleteJSONBody = { messages: messageIds };
+	if (messages.length === 1) {
+		const headers = new Headers({
+			Authorization: `Bot ${env.DISCORD_BOT_TOKEN}`,
+			"X-Audit-Log-Reason": auditLogReason,
+		});
+		const url = `https://discord.com/api/v${APIVersion}/channels/${env.DISCORD_CHANNEL_ID}/messages/${messages[0].id}`;
+		const init: RequestInit = {
+			method: "DELETE",
+			headers,
+		};
+		const response = await fetch(url, init);
+		if (response.status !== emptyResponse) {
+			throw new Error(`Error deleting previous message, status: ${response.status}`);
+		}
+	} else {
+		const headers = new Headers({
+			Authorization: `Bot ${env.DISCORD_BOT_TOKEN}`,
+			"Content-Type": "application/json",
+			"X-Audit-Log-Reason": auditLogReason,
+		});
 
-	const url = `https://discord.com/api/v${APIVersion}/channels/${env.DISCORD_CHANNEL_ID}/messages/bulk-delete`;
-	const init: RequestInit = {
-		method: "POST",
-		headers,
-		body: JSON.stringify(payload),
-	};
+		const messageIds: string[] = [];
+		messages.forEach((message) => {
+			messageIds.push(message.id);
+		});
+		const payload: RESTPostAPIChannelMessagesBulkDeleteJSONBody = { messages: messageIds };
 
-	const response = await fetch(url, init);
-	if (response.status !== emptyResponse) {
-		throw new Error(`Error deleting previous message, status: ${response.status}`);
+		const url = `https://discord.com/api/v${APIVersion}/channels/${env.DISCORD_CHANNEL_ID}/messages/bulk-delete`;
+		const init: RequestInit = {
+			method: "POST",
+			headers,
+			body: JSON.stringify(payload),
+		};
+
+		const response = await fetch(url, init);
+		if (response.status !== emptyResponse) {
+			throw new Error(`Error deleting previous message, status: ${response.status}`);
+		}
 	}
 }
